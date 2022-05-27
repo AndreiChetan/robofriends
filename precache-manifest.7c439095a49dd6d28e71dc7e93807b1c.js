ece327d2dbbe7489e30d15268963678da552fca5
Chetzy <chetanandrei1@gmail.com>
2022-05-27 13:11:06 +0300

    Updates

---
+++ precache-manifest.7c439095a49dd6d28e71dc7e93807b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "15c908346201977acbb1",
+    "url": "/robofriends/static/css/main.bfd73861.chunk.css"
+  },
+  {
+    "revision": "15c908346201977acbb1",
+    "url": "/robofriends/static/js/main.15c90834.chunk.js"
+  },
+  {
+    "revision": "63b9bce215fa207d86c7",
+    "url": "/robofriends/static/css/1.8c65fc9a.chunk.css"
+  },
+  {
+    "revision": "63b9bce215fa207d86c7",
+    "url": "/robofriends/static/js/1.63b9bce2.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1a5a02d4403068129ed9f8eabfa6125f",
+    "url": "/robofriends/index.html"
+  }
+];
